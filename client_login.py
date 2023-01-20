92dab7e64c71a07a9f62bd35e48404aa398a9e9a
TheCoderBroSr <arhantsurajkumar@gmail.com>
2023-01-20 14:13:42 +0530

    Added a new GUI file for the user to login & chat

---
+++ client_login.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import ttk
+from chat_window import start_client
+
+def on_submit():
+    username = name_entry.get()
+    root.destroy()
+    start_client(username)
+    
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    root.title("Chat Client - Enter User Name")
+
+    mainframe = ttk.Frame(root, padding="3 3 12 12")
+    mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+    mainframe.columnconfigure(0, weight=1)
+    mainframe.rowconfigure(0, weight=1)
+
+    name = tk.StringVar()
+
+    name_entry = ttk.Entry(mainframe, width=7, textvariable=name)
+    name_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
+
+    ttk.Label(mainframe, text="User Name").grid(column=1, row=1, sticky=tk.W)
+    ttk.Button(mainframe, text="Submit", command=on_submit).grid(column=3, row=3, sticky=tk.W)
+
+    for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+    name_entry.focus()
+    root.bind('<Return>', lambda event: on_submit())
+    root.mainloop()
