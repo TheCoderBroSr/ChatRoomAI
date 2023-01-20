5d790ddbb4fc2099b50d269e6cb29b063099d1a6
TheCoderBroSr <arhantsurajkumar@gmail.com>
2023-01-20 14:13:22 +0530

    Changed the file to be a window function file

---
+++ chat_window.py
@@ -0,0 +1,95 @@
+import socket
+import sys
+import tkinter as tk
+from tkinter import ttk
+from threading import Thread
+from queue import Queue
+
+
+def receive(queue):
+    while True:
+        try:
+            data = client_socket.recv(1024).decode()
+            if data:
+                queue.put(data)
+        except:
+            break
+
+
+def update_text(queue):
+    while True:
+        try:
+            data = queue.get(timeout=0.1)
+
+            text.config(state='normal')
+            
+            # Assigning different tags, to different types of text
+            if "has joined" in data:
+                tag = "joined"
+            elif "has disconnected" in data:
+                tag = "disconnected"
+            else:
+                tag = data[0:data.index(':')] # Getting Username from data
+
+            tag += "_tag"
+            text.insert(tk.END, data + '\n', tag)
+
+            text.config(state='disabled')
+        except:
+            root.after(1000, update_text, queue)
+            break
+
+
+def send_message():
+    message = entry.get()
+    client_socket.send(message.encode())
+    entry.delete(0, 'end')
+
+
+def on_closing():
+    client_socket.close()
+    root.destroy()
+    sys.exit()
+
+
+def start_client(username:str):
+    global client_socket, text, entry, root
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = socket.gethostname()
+    port = 12345
+
+    client_socket.connect((host, port))
+    client_socket.send(username.encode())
+
+    queue = Queue()
+    receive_thread = Thread(target=receive, args=(queue,))
+    receive_thread.start()
+
+    root = tk.Tk()
+    root.title("Chat Client")
+
+    # Esc and q key for ending the program
+    root.bind("<Escape>", lambda event: root.destroy())
+    root.bind("q", lambda event: on_closing())
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    
+    text = tk.Text(root) 
+    text.config(font=("Courier", 12), background='#f0f0f0', state='disabled')
+    text.tag_config("joined_tag", foreground="green")
+    text.tag_config("disconnected_tag", foreground="red")
+    text.tag_config(f"{username}_tag", foreground="blue")
+    text.pack()
+
+    entry = tk.Entry(root, width=40)
+    entry.config(font=("Courier", 12), background='#f0f0f0')
+    entry.bind("<Return>", lambda event: send_message())
+    entry.pack()
+
+    style = ttk.Style()
+    style.configure("TButton", font="Verdana 15", padding=6, relief="flat", background="#ccc")
+    send_button = ttk.Button(root, text="Send", style="TButton", command=send_message)
+    send_button.pack()
+
+    root.after(0, update_text, queue)
+    root.mainloop()
+
